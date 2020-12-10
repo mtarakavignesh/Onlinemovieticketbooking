6f24f811d9135e774abaa10da68d49fb849b7cd6
Taraka Vignesh Mullapudi <mtarakavignesh@gmail.com>
2020-12-10 11:11:25 +0530

    Update app.js

--- onlineticketbookingservice2/app.js
+++ onlineticketbookingservice2/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var monk = require('monk');
-var db = //use yout mongo db uri//
+var db = //use your mongo db uri//
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
